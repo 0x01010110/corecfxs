2021d0caee1a8e53827951301771661101a0ce90
0x0x0x0xx0 <0xx0x0xx0@gmail.com>
2023-12-14 17:49:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+const { Conflux, Drip } = require('js-conflux-sdk');
+
+const conflux = new Conflux({
+    url: 'https://main.confluxrpc.com',
+    networkId: 1029,
+});
+
+const privateKey = require('./config.json').privateKey;
+const account = conflux.wallet.addPrivateKey(privateKey);
+
+const CrossSpaceCall = conflux.InternalContract('CrossSpaceCall');
+
+async function main() {
+    
+    while(true) {
+        try {
+            await oneRound();
+            console.log('One round finished');
+        } catch(err) {
+            console.error(err);
+        }
+        await waitMilliseconds(10 * 1000);
+    }
+
+}
+
+async function waitMilliseconds(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function oneRound() {
+    let hash;
+    let batch = 5;
+    let nonce = await conflux.getNextNonce(account.address);
+    // let nonce = await conflux.txpool.nextNonce(account.address);
+    for(let i = 0; i < batch; i++) {
+        try {
+            hash = await CrossSpaceCall.transferEVM('0xc6e865c213c89ca42a622c5572d19f00d84d7a16').sendTransaction({
+                from: account.address,
+                nonce: nonce + BigInt(i),
+                // gasPrice: Drip.fromGDrip(5),  // call also specify the gasPrice
+            });
+            console.log(`Sending ${i}`, hash);
+        } catch(err) {
+            console.error(err);
+            break;
+        }
+        // await waitMilliseconds(100);
+    }
+
+    for(let i = 0; i < 30; i++) {
+        try {
+            if (!hash) break;
+            let receipt = await conflux.getTransactionReceipt(hash);
+            if (receipt){
+                break;
+            }
+            await waitMilliseconds(1000);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+}
+
+main().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# corecfxs
+
+本脚本可以通过跨 space 打铭文
+
+## 使用方法
+
+1. 安装 node.js 环境
+2. 使用 git 下载本项目: `git clone https://github.com/0x01010110/corecfxs.git`
+3. npm install
+4. 修改 config.json 文件的 `privateKey` 填入私钥. 需要确保账户里有足够的 CFX 用于支付手续费 
+5. 启动脚本  `node index.js`
