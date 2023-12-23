b263b49b7bc672ad3d1b5e575780e14b15e350da
0x0x0x0xx0 <0xx0x0xx0@gmail.com>
2023-12-23 20:50:25 +0800

    fix bug

--- exchangeCfxs.js
+++ exchangeCfxs.js
@@ -6,27 +6,34 @@ const mappedAddress = address.cfxMappedEVMSpaceAddress(account.address);
 
 async function main() {
     const ids = await getIDs(mappedAddress);
-    for(let id of ids) {
-        if (id === '0') continue;
+    const step = 5;
+    for(let i = 0; i < ids.length; i += step) {
         try {
-            let cfxsId = parseInt(id);
-            // check owner
-            let info = await cfxsContract.CFXss(cfxsId);
-            if(!info || info.length === 0 || info[1] != mappedAddress) {
-                console.log(`Id ${cfxsId} is not yours`);
-                await waitMilliseconds(100);
-                continue;
+            let exIds = [];
+            for(let j = 0; j < step; j++) {
+                let id = ids[i + j];
+                if (id === '0') continue;
+                let cfxsId = parseInt(id);
+                // check owner
+                let info = await cfxsContract.CFXss(cfxsId);
+                if(!info || info.length === 0 || info[1] != mappedAddress) {
+                    console.log(`Id ${cfxsId} is not yours`);
+                    await waitMilliseconds(100);
+                    continue;
+                }
+                let minted = await cfxsExchangeContract.minted(cfxsId);
+                if (minted) {
+                    console.log(`Id ${cfxsId} already exchanged`);
+                    await waitMilliseconds(100);
+                    continue;
+                }
+                exIds.push(cfxsId);
             }
-            let minted = await cfxsExchangeContract.minted(cfxsId);
-            if (minted) {
-                console.log(`Id ${cfxsId} already exchanged`);
-                await waitMilliseconds(100);
-                continue;
-            }
-
-            console.log(`Exchange cfxs id ${cfxsId}`);
-            const receipt = await exchangeCFXs([cfxsId]);
+            if (exIds.length === 0) continue;
+            console.log(`Exchange cfxs id ${exIds}`);
+            const receipt = await exchangeCFXs(exIds);
             console.log(`Result: ${receipt.outcomeStatus === 0 ? 'success' : 'fail'}`);
+            console.log(receipt.transactionHash);
         } catch(e) {
             console.log('Transfer Error', e);
             await waitMilliseconds(500);
@@ -47,7 +54,7 @@ async function getIDs(_addr) {
     const limit = 1000;
     let startIndex = 0;
     while(true) {
-        let { count, rows } = await axios.get(`http://test.conins.io/?owner=${address}&startIndex=${startIndex}&size=${limit}`);
+        let { data: {count, rows} } = await axios.get(`http://test.conins.io/?owner=${_addr}&startIndex=${startIndex}&size=${limit}`);
         ids = ids.concat(rows.map(item => item.id));
         if (rows.length < limit) {
             break;
