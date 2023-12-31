3b0572bfb60176b05e3822596720996fa85b3cc8
0x0x0x0xx0 <0xx0x0xx0@gmail.com>
2023-12-31 23:02:13 +0800

    misc

--- index-data/index-by-read-contract.js
+++ index-data/index-by-read-contract.js
@@ -1,23 +1,23 @@
-const {cfxsContract} = require('../conflux.js');
+const {cfxsMainContract} = require('../conflux.js');
 const { waitMilliseconds } = require('../utils.js');
 const fs = require('fs');
-let result = require('./cfxs-transfered-ids.json');
+let result = {};
 
-const START_ID = 20999965;
+const START_ID = 0;
 const END_ID = 21008206;
 
 async function main() {
     for(let id = START_ID; id <= END_ID; id++) {
         try {
             if (result[id]) continue;
-            const info = await cfxsContract.CFXss(id);
+            const info = await cfxsMainContract.CFXss(id);
             const owner = info[1];
             result[id] = owner;
             console.log(id, owner);
         } catch(e) {
             console.log('Failed id===================', id, e);
         }
-        await waitMilliseconds(100);
+        await waitMilliseconds(50);
     }
     fs.writeFileSync('./index-data/cfxs-transfered-ids.json', JSON.stringify(result, null, '\t'));
     console.log('Finished');
